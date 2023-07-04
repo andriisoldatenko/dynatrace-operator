02e549cdacc9ccb5c50080216062d1b24eadbb29
Marcell Sevcsik <marcell.sevcsik@dynatrace.com>
2023-07-04 15:55:22 +0200

    Adds clean up for dynakubes

--- src/controllers/csi/metadata/correctness.go
+++ src/controllers/csi/metadata/correctness.go
@@ -2,8 +2,10 @@ package metadata
 
 import (
 	"context"
+	"os"
 
 	dynatracev1beta1 "github.com/Dynatrace/dynatrace-operator/src/api/v1beta1"
+	dtcsi "github.com/Dynatrace/dynatrace-operator/src/controllers/csi"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -13,17 +15,20 @@ import (
 // Removes not valid entries
 func CorrectMetadata(ctx context.Context, cl client.Client, access Access) error {
 	defer LogAccessOverview(access)
-	if err := correctVolumes(ctx, cl, access); err != nil {
+	if err := removeVolumesForMissingPods(ctx, cl, access); err != nil {
 		return err
 	}
-	if err := correctDynakubes(ctx, cl, access); err != nil {
+	if err := removeMissingDynakubes(ctx, cl, access); err != nil {
+		return err
+	}
+	if err := removeDynakubesWithVersionForDeprecatedBin(ctx, cl, access); err != nil {
 		return err
 	}
 	return nil
 }
 
 // Removes volume entries if their pod is no longer exists
-func correctVolumes(ctx context.Context, cl client.Client, access Access) error {
+func removeVolumesForMissingPods(ctx context.Context, cl client.Client, access Access) error {
 	podNames, err := access.GetPodNames(ctx)
 	if err != nil {
 		return err
@@ -40,12 +45,12 @@ func correctVolumes(ctx context.Context, cl client.Client, access Access) error 
 		}
 		pruned = append(pruned, volumeID+"|"+podName)
 	}
-	log.Info("CSI volumes database is corrected (volume|pod)", "prunedRows", pruned)
+	log.Info("CSI volumes database is corrected for missing pods (volume|pod)", "prunedRows", pruned)
 	return nil
 }
 
 // Removes dynakube entries if their Dynakube instance no longer exists in the cluster
-func correctDynakubes(ctx context.Context, cl client.Client, access Access) error {
+func removeMissingDynakubes(ctx context.Context, cl client.Client, access Access) error {
 	dynakubes, err := access.GetTenantsToDynakubes(ctx)
 	if err != nil {
 		return err
@@ -62,6 +67,29 @@ func correctDynakubes(ctx context.Context, cl client.Client, access Access) erro
 		tenantUUID := dynakubes[dynakubeName]
 		pruned = append(pruned, tenantUUID+"|"+dynakubeName)
 	}
-	log.Info("CSI tenants database is corrected (tenant|dynakube)", "prunedRows", pruned)
+	log.Info("CSI tenants database is corrected for missing dynakubes (tenant|dynakube)", "prunedRows", pruned)
+	return nil
+}
+
+func removeDynakubesWithVersionForDeprecatedBin(ctx context.Context, cl client.Client, access Access) error {
+	path := PathResolver{RootDir: dtcsi.DataPath} // TODO: make better
+	dynakubes, err := access.GetAllDynakubes(ctx)
+	if err != nil {
+		return err
+	}
+	pruned := []string{}
+	for _, dynakube := range dynakubes {
+		if dynakube.TenantUUID == "" || dynakube.LatestVersion == "" {
+			continue
+		}
+		_, err := os.Stat(path.AgentBinaryDirForVersion(dynakube.TenantUUID, dynakube.LatestVersion))
+		if err == nil {
+			if err := access.DeleteDynakube(ctx, dynakube.Name); err != nil {
+				return err
+			}
+			pruned = append(pruned, dynakube.TenantUUID+"|"+dynakube.Name)
+		}
+	}
+	log.Info("CSI tenants database is corrected for deprecated agent binary location (tenant|dynakube)", "prunedRows", pruned)
 	return nil
 }
