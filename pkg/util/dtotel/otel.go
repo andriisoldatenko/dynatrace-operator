48b8bb8241d8c96bfd2ddc4ca778ccabc644454c
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-07-11 08:17:06 +0200

    try with sourceURL

--- pkg/util/dtotel/otel.go
+++ pkg/util/dtotel/otel.go
@@ -9,7 +9,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/metric/noop"
 	"go.opentelemetry.io/otel/sdk/resource"
-	// renovate datasource=https://github.com/open-telemetry/semantic-conventions
+	// renovate depName=go.opentelemetry.io/otel/semconv
 	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
 	"go.opentelemetry.io/otel/trace"
 	"k8s.io/apimachinery/pkg/types"
