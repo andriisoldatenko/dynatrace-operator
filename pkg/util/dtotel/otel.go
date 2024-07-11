c24299420da2322fec76e18397e0eda52e0bd548
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-07-11 09:20:20 +0200

    try "github-releases"

--- pkg/util/dtotel/otel.go
+++ pkg/util/dtotel/otel.go
@@ -9,7 +9,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/metric/noop"
 	"go.opentelemetry.io/otel/sdk/resource"
-	// renovate packageName=go.opentelemetry.io/otel/semconv depName=github.com/open-telemetry/semantic-conventions
+	// renovate packageName=open-telemetry/semantic-conventions depName=github.com/open-telemetry/semantic-conventions
 	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
 	"go.opentelemetry.io/otel/trace"
 	"k8s.io/apimachinery/pkg/types"
