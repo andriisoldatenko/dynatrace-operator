c78dc16fb5e1041e6aa1d9dd2eb0c417bd7d11d7
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-06 19:00:28 +0100

    remove hardcode image

--- hack/build/ci/create-manifest.sh
+++ hack/build/ci/create-manifest.sh
@@ -12,6 +12,8 @@ image_tag=$2
 multiplatform=$3
 image="${image_name}:${image_tag}"
 
+
+
 if [ "$multiplatform" == "true" ]
 then
 #  supported_architectures=("amd64" "arm64" "ppc64le" "s390x")
@@ -25,17 +27,21 @@ then
     images+=("${image}-${architecture}")
   done
   docker manifest create "${image}" "${images[@]}"
+  if [[ "$image" =~ gcr.io ]]
+  then
+    go install github.com/google/go-containerregistry/cmd/crane@latest
+    crane mutate "${image}" --annotation "com.googleapis.cloudmarketplace.product.service.name=services/dynatrace-operator-dynatrace-marketplace-prod.cloudpartnerservices.goog"
+  fi
 else
   echo "Creating manifest for the AMD image "
   docker pull "${image}-amd64"
   docker manifest create "${image}" "${image}-amd64"
+  if [[ "$image" =~ gcr.io ]]
+  then
+    go install github.com/google/go-containerregistry/cmd/crane@latest
+    crane mutate "${image}" --annotation "com.googleapis.cloudmarketplace.product.service.name=services/dynatrace-operator-dynatrace-marketplace-prod.cloudpartnerservices.goog"
+  fi
 fi
 
 sha256=$(docker manifest push "${image}")
 echo "digest=${sha256}">> "$GITHUB_OUTPUT"
-
-if [[ "$image" =~ gcr.io ]]
-then
-  go install github.com/google/go-containerregistry/cmd/crane@latest
-  crane mutate "${image}" --annotation "com.googleapis.cloudmarketplace.product.service.name=services/dynatrace-operator-dynatrace-marketplace-prod.cloudpartnerservices.goog"
-fi
