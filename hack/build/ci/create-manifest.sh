1ce75fac033d3b9b0b722c30adbfedd89c125040
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2025-03-10 10:17:42 +0100

    fixup!

--- hack/build/ci/create-manifest.sh
+++ hack/build/ci/create-manifest.sh
@@ -35,11 +35,11 @@ else
 fi
 
 sha256=$(docker manifest push "${image}")
-if [[ "$image" =~ gcr.io ]]
-then
-  go install github.com/google/go-containerregistry/cmd/crane@latest
-  docker version
-  crane mutate --verbose "${image}" --annotation "com.googleapis.cloudmarketplace.product.service.name=services/dynatrace-operator-dynatrace-marketplace-prod.cloudpartnerservices.goog"
-  docker manifest inspect "${image}"
-fi
+#if [[ "$image" =~ gcr.io ]]
+#then
+#  go install github.com/google/go-containerregistry/cmd/crane@latest
+#  docker version
+#  crane mutate --verbose "${image}" --annotation "com.googleapis.cloudmarketplace.product.service.name=services/dynatrace-operator-dynatrace-marketplace-prod.cloudpartnerservices.goog"
+#  docker manifest inspect "${image}"
+#fi
 echo "digest=${sha256}">> "$GITHUB_OUTPUT"
